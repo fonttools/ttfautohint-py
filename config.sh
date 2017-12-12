b3066e11f88ff43fe53f0bef3a2f1c983309e5c3
Cosimo Lupo <cosimo@anthrotype.com>
2017-12-12 17:05:37 +0000

    try to install autotools bison flex ragel

--- config.sh
+++ config.sh
@@ -4,7 +4,21 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    :
+    if [ -n "$IS_OSX" ]; then
+        brew install autoconf
+        brew install automake
+        brew install libtool
+        brew install bison
+        brew install flex
+        brew install ragel
+    else
+        sudo yum install autoconf
+        sudo yum install automake
+        sudo yum install libtool
+        sudo yum install bison
+        sudo yum install flex
+        sudo yum install ragel
+    fi
 }
 
 function run_tests {
