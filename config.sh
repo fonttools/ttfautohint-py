72566d730c0e59c2eb99de1f868df6698aa5b75e
Cosimo Lupo <cosimo@anthrotype.com>
2017-12-13 10:11:03 +0000

    travis: debug PATH issues on osx build

--- config.sh
+++ config.sh
@@ -2,17 +2,31 @@
 # Test for OSX with [ -n "$IS_OSX" ]
 
 if [ -n "$IS_OSX"]; then
-	export PATH="/usr/local/opt/bison/bin/:/usr/local/opt/flex/bin/:/usr/local/opt/libtool/bin/:$PATH"
+    PATH="/usr/local/opt/libtool/bin:$PATH"
+    PATH="/usr/local/opt/ragel/bin:$PATH"
+    PATH="/usr/local/opt/bison/bin:$PATH"
+    PATH="/usr/local/opt/flex/bin:$PATH"
+    export PATH
 fi
 
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    if [ -z "$IS_OSX" ]; then
-		build_simple libtool 2.4.6 http://ftpmirror.gnu.org/libtool
-		build_simple ragel 6.10 http://www.colm.net/files/ragel
-		build_simple flex 2.6.4 https://github.com/westes/flex/releases/download/v2.6.4
-		build_simple bison 3.0.4 http://ftp.gnu.org/gnu/bison
+    if [ -n "$IS_OSX" ]; then
+        echo $PATH
+        which libtool
+        libtool --version
+        which ragel
+        ragel --version
+        which flex
+        flex --version
+        which bison
+        bison --version
+    else
+        build_simple libtool 2.4.6 http://ftpmirror.gnu.org/libtool
+        build_simple ragel 6.10 http://www.colm.net/files/ragel
+        build_simple flex 2.6.4 https://github.com/westes/flex/releases/download/v2.6.4
+        build_simple bison 3.0.4 http://ftp.gnu.org/gnu/bison
     fi
 }
 
