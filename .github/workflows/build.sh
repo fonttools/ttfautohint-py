5df00782c7b90aeee3e2d8df40c6f676b36f0c92
Simon Cozens <simon@simon-cozens.org>
2021-06-15 11:12:30 +0100

    Attempt to use github workflow

---
+++ .github/workflows/build.sh
@@ -0,0 +1,32 @@
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  # these cause a conflict with built webp and libtiff,
+  # curl from brew requires zstd, use system curl
+  brew remove --ignore-dependencies webp zstd xz libtiff curl
+fi
+
+if [[ "$MB_PYTHON_VERSION" == pypy3* ]]; then
+  if [[ "$TRAVIS_OS_NAME" != "macos-latest" ]]; then
+    MB_ML_VER="2010"
+    DOCKER_TEST_IMAGE="multibuild/xenial_$PLAT"
+  else
+    MB_PYTHON_OSX_VER="10.9"
+  fi
+fi
+
+echo "::group::Install a virtualenv"
+  source multibuild/common_utils.sh
+  source multibuild/travis_steps.sh
+  python3 -m pip install virtualenv
+  before_install
+echo "::endgroup::"
+
+echo "::group::Build wheel"
+  build_wheel $REPO_DIR $PLAT
+  ls -l "${GITHUB_WORKSPACE}/${WHEEL_SDIR}/"
+echo "::endgroup::"
+
+if [[ $MACOSX_DEPLOYMENT_TARGET != "11.0" ]]; then
+  echo "::group::Test wheel"
+    install_run $PLAT
+  echo "::endgroup::"
+fi
