89137e20c95d26295da14ea5f87371d853d30a56
Simon Cozens <simon@simon-cozens.org>
2022-03-07 15:35:51 +0000

    Fix M1 segfault

--- src/python/ttfautohint/__init__.py
+++ src/python/ttfautohint/__init__.py
@@ -58,6 +58,8 @@ class TALibrary(object):
         version_string = lib.TTF_autohint_version_string().decode('ascii')
         self.version_string = version_string
 
+        lib.TTF_autohint.argtypes = [c_char_p]
+
     def _build_info_data(self, options):
         # as a side effect, these arguments are popped from the options dict
         # as they are not part of TTF_autohint API
