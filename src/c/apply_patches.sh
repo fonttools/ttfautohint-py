88b0249261a8e4c8b8092ddaf4273bdf6e957909
Cosimo Lupo <clupo@google.com>
2025-07-28 16:13:22 +0100

    Revert "fix apply_patches.sh pointing to renamed freetype src dir"
    This reverts commit 1e1d7d7fd31f454b61f97d50a6dc909edf4f243b.

--- src/c/apply_patches.sh
+++ src/c/apply_patches.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 FREETYPE2_PATCH="${SCRIPT_DIR}/freetype2.patch"
 HARFBUZZ_PATCH="${SCRIPT_DIR}/harfbuzz.patch"
 
-cd "${SCRIPT_DIR}/freetype"
+cd "${SCRIPT_DIR}/freetype2"
 git apply "${FREETYPE2_PATCH}"
 
 cd "${SCRIPT_DIR}/harfbuzz"
